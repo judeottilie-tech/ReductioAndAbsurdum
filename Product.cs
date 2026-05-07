8f5f8446d5bfc02d209b2f4e891f285d150557f4
Jude Andersen <jude.ottilie@gmail.com>
2026-05-07 15:03:13 -0500

    save

--- Product.cs
+++ Product.cs
@@ -1,10 +1,26 @@
+
+public class ProductType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
 public class Product
 {
     public string Name { get; set; }
     public decimal Price { get; set; }
     public bool Sold { get; set; }
     public int ProductTypeId { get; set; }
-    public DateTime DaysOnShelf { get; set; }
+    public DateTime DateEntered { get; set; }
+
+    public int DaysOnShelf
+    {
+        get
+        {
+            return (DateTime.Now - DateEntered).Days;
+        }
+    }
+}
+   // public DateTime DaysOnShelf { get; set; }
 
     /* public int DaysOnShelf
     {
@@ -14,4 +30,3 @@ public class Product
         }
     }
     */
-}
--- Program.cs
+++ Program.cs
@@ -1,53 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
+List<ProductType> productTypes = new List<ProductType>()
+{
+    new ProductType() { Id = 1, Name = "Apparel" },
+    new ProductType() { Id = 2, Name = "Potions" },
+    new ProductType() { Id = 3, Name = "Enchanted Objects" },
+    new ProductType() { Id = 4, Name = "Wands" }
+};
+
 List<Product> products = new List<Product>()
 {
     new Product()
     {
         Name = "Wizard Hat",
-        Price = 10,
+        Price = 10.00M,
         Sold = true,
-        //ProductTypeId = ,
-        DaysOnShelf = new DateTime(2026, 1, 1)
+        ProductTypeId = 1,
+        DateEntered = new DateTime(2026, 2, 28)
     },
     new Product()
     {
         Name = "Purple Cloak",
-        Price = 50,
-        Sold = true,
-   //     ProductTypeId = ,
-        DaysOnShelf = new DateTime(2026, 1, 1)
+        Price = 50.00M,
+        Sold = false,
+        ProductTypeId = 1,
+        DateEntered = new DateTime(2026, 1, 30)
     },
     new Product()
     {
         Name = "Birch Wand",
-        Price = 20,
-        Sold = false,
-        //ProductTypeId = ,
-        DaysOnShelf = new DateTime(2026, 1, 1)
+        Price = 20.00M,
+        Sold = true,
+        ProductTypeId = 4,
+        DateEntered = new DateTime(2026, 3, 12)
     },
     new Product()
     {
         Name = "Potion of Invisibility",
-        Price = 5,
+        Price = 5.00M,
         Sold = true,
-        //ProductTypeId = ,
-        DaysOnShelf = new DateTime(2026, 1, 1)
+        ProductTypeId = 2,
+        DateEntered = new DateTime(2025, 11, 22)
     },
     new Product()
     {
         Name = "Love Potion",
-        Price = 500,
+        Price = 500.00M,
         Sold = true,
-        //ProductTypeId = ,
-        DaysOnShelf = new DateTime(2026, 1, 1)
+        ProductTypeId = 2,
+        DateEntered = new DateTime(2026, 5, 1)
     },
     new Product()
     {
         Name = "Lucky Charm",
-        Price = 10,
+        Price = 10.00M,
         Sold = true,
-        //ProductTypeId = ,
-        DaysOnShelf = new DateTime(2026, 1, 1)
+        ProductTypeId = 3,
+        DateEntered = new DateTime(2026, 5, 1)
     },
     
 };
@@ -56,44 +64,150 @@ string greeting = @"Welcome to Reductio & Absurdum!
 Providing high-quality magical supplies to the wizarding community for nearly 1000 years at their shop on Calendula Road.";
 Console.WriteLine(greeting);
 
-/*
-Console.WriteLine(@"
+
+string choice = null;
+while (choice != "0")
+{
+    Console.WriteLine(@"
 Menu:
 0. Exit
 1. View All Products
 2. View Products for a Category
-3. Add a Product  to the Inventory
+3. Add a Product to the Inventory
 4. Delete a Product from the Inventory
 5. Update a Product's Details");
 
+/*
+0. Exit
+1. Apparel
+2. Potion
+3. Enchanted Item
+4. Wand
+
+
+ */
+
+choice = Console.ReadLine().Trim();
+
+    if (choice == "0")
+    {
+        Console.WriteLine("Goodbye");
+    }
+    else if (choice == "1")
+    {
+        ListProducts();
+    }
+   
+    else if (choice == "2")
+    {
+        ViewByCategory();
+    }
+
+     /*
+    else if (choice == "3")
+    {
+        AddProduct();
+    }
+    else if (choice == "4")
+    {
+        DeleteProduct();
+    }
+    else if (choice == "5")
+    {
+        UpdateProduct();
+    }
+    */
+}
+
+void ListProducts()
+{
+    Console.WriteLine($"Products:");
+
+    for (int i = 0; i < products.Count; i++)
+    {
+    Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+}
+
+void ViewByCategory()
+{
+    Console.WriteLine($"Item Categories:");
+
+    for (int i = 0; i < productTypes.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {productTypes[i].Name}");
+    };
+
+    Console.WriteLine("Enter category number: ");
+    int response = int.Parse(Console.ReadLine().Trim());
+
+    while (response > productTypes.Count || response < 1)
+    {
+    Console.WriteLine($"Choose a number between 1 and {productTypes.Count}!");
+    response = int.Parse(Console.ReadLine().Trim());
+    };
+
+    List<Product> filtered = products.Where(product => product.ProductTypeId == productTypes[response - 1].Id).ToList();
+
+    if (filtered.Count == 0)
+    {
+        Console.WriteLine("No products in this category");
+        return;
+    }
+
+    ProductType selectedType = productTypes[response - 1];
+    Console.WriteLine($"{selectedType.Name}:");
+    foreach (Product product in filtered)
+    {
+        Console.WriteLine($"{product.Name}, ${product.Price}, {product.DaysOnShelf} days on shelf");
+    }
+}
+/*
+   products.Where(p => p.ProductTypeId == productTypes[catChoice - 1].Id).ToList();
+
+    if (filtered.Count == 0)
+    {
+        Console.WriteLine("No products in that category");
+    }
+
+    Console.WriteLine($"{type.Name}, ${type.Price}, ${type.DaysOnShelf} days on shelf");
+
+    string productTypeNames productTypes.Where(t => t.Name()).ToList();
+
+    foreach (string ProductType in productTypeNames)
+    {
+        Console.WriteLine(ProductType);
+    }
+    */
+
+
+    
+
+
+
+/*
 Console.WriteLine(@"Products:
 1 apparel
 2. potions
 3. enchanted objects
 4. wands
-5. dog");*/
+5. dog");
 
-Console.WriteLine("Products:");
+Console.WriteLine("Please enter a category number: ");
 
-for (int i = 0; i < products.Count; i++)
-{
-    Console.WriteLine($"{i + 1}. {products[i].Name}");
-}
-
-Console.WriteLine("Please enter a product number: ");
 int response = int.Parse(Console.ReadLine().Trim());
-
-    Product chosenProduct = products[response - 1];
 
 while (response > products.Count || response < 1)
 {
-    Console.WriteLine("Choose a number between 1 and 5!");
+    Console.WriteLine($"Choose a number between 1 and {products.Count}!");
     response = int.Parse(Console.ReadLine().Trim());
 }
 
+Product chosenProduct = products[response - 1];
+
 DateTime now = DateTime.Now;
 
-TimeSpan timeInStock = now - chosenProduct.DaysOnShelf;
+TimeSpan timeInStock = now - chosenProduct.DateEntered;
 
 
     Console.WriteLine($"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars and is {(chosenProduct.Sold ? "" : "not ")}sold.");
